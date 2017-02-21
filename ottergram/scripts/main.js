b44c3d0a93b643e2530f87a54806f52805874479
Patti Scalfano <pscalfano@hotmail.com>
2017-02-21 12:16:52 -0500

    Chapter 6 Gold and silver challenge

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+// Gold challenge
+var RANDOM_IMAGE = 'https://f4.bcbits.com/img/a0683688743_10.jpg';
+var lastimagechanged;
 
 function setDetails(imageUrl, titleText) {
     'use strict';
@@ -30,6 +33,10 @@ function addThumbClickedHandler(thumb) {
   thumb.addEventListener('click', function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    if (thumb.getAttribute('data-image-url') == RANDOM_IMAGE) {
+      thumb.setAttribute('data-image-url',lastimagechanged);
+      changeRandomUrl();
+    }
   });
 }
 function getThumbnailsArray() {
@@ -44,3 +51,33 @@ function initializeEvents() {
   thumbnails.forEach(addThumbClickedHandler);
 }
 initializeEvents();
+
+// Silver challenge. Enter the two functions into the Console
+// and then run KillLinks();
+function addAnchorClickedHandler(anchor) {
+  'use strict';
+  anchor.addEventListener('click', function(event) {
+    event.preventDefault();
+  });
+}
+function KillLinks() {
+  var anchors = document.getElementsByTagName('a');
+  var anchorArray = [].slice.call(anchors);
+  anchorArray.forEach(addAnchorClickedHandler);
+}
+
+// Gold Challenge
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function changeRandomUrl() {
+  var imageToChange = getRandomIntInclusive(0,4);
+  // Just to see which one changes
+  console.log('image changed is ' + imageToChange);
+  var thumbnails = getThumbnailsArray();
+  lastimagechanged = thumbnails[imageToChange].getAttribute('data-image-url');
+  thumbnails[imageToChange].setAttribute('data-image-url',RANDOM_IMAGE);
+}
+changeRandomUrl();

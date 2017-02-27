4a2044a64fa3a219d6c9234b3dc88751497d7166
Patti Scalfano <pscalfano@hotmail.com>
2017-02-27 10:39:31 -0500

    Ottergram complete - end of Chapter 7

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,9 +1,14 @@
+'use strict';
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
-    'use strict';
+    // 'use strict';
     var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
     detailImage.setAttribute('src', imageUrl);
     var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
@@ -11,36 +16,59 @@ function setDetails(imageUrl, titleText) {
 }
 
 function imageFromThumb(thumbnail) {
-    'use strict';
+    // 'use strict';
     return thumbnail.getAttribute('data-image-url');
 }
 
 function titleFromThumb(thumbnail) {
-    'use strict';
+    // 'use strict';
     return thumbnail.getAttribute('data-image-title');
 }
 
 function setDetailsFromThumb(thumbnail) {
-    'use strict';
+    // 'use strict';
     setDetails(imageFromThumb(thumbnail),titleFromThumb(thumbnail));
 
 }
 function addThumbClickedHandler(thumb) {
-  'use strict';
+  // 'use strict';
   thumb.addEventListener('click', function(event) {
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();
   });
 }
 function getThumbnailsArray() {
-  'use strict';
+  // 'use strict';
   var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
   var thumbnailArray= [].slice.call(thumbnails);
   return thumbnailArray;
 }
+function hideDetails() {
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+function showDetails() {
+  var frame=document.querySelector(DETAIL_FRAME_SELECTOR);
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function() {
+     frame.classList.remove(TINY_EFFECT_CLASS)
+   },50);
+
+}
+function addKeyPressHandler() {
+  document.body.addEventListener('keyup',function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if (event.keyCode === ESC_KEY) {
+        hideDetails();
+    }
+  })
+}
 function initializeEvents() {
-  'use strict';
+  // 'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickedHandler);
+  addKeyPressHandler();
 }
 initializeEvents();
